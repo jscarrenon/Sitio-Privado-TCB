6288b5e9e8b1791040854410618583adb29b4f62
Nicolás Torres <ntorres@kunder.cl>
2015-12-24 12:21:23 -0300

    Elimina scripts de proyecto de ejemplo y traspasa js y css a bundles

--- Sitio Privado/App_Start/BundleConfig.cs
+++ Sitio Privado/App_Start/BundleConfig.cs
@@ -8,16 +8,33 @@ namespace Sitio_Privado
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //CSS
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/lib/components-font-awesome/css/font-awesome.min.css"
+                    ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //Scripts Angular
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Content/lib/angular/angular.js",
+                        "~/Content/lib/angular-animate/angular-animate.min.js",
+                        "~/Content/lib/angular-aria/angular-aria.min.js",
+                        "~/Content/lib/angular-i18n/angular-locale_es-cl.js",
+                        "~/Content/lib/angular-messages/angular-messages.min.js",
+                        "~/Content/lib/angular-resource/angular-resource.min.js",
+                        "~/Content/lib/angular-ui-router/release/angular-ui-router.min.js"
+                    ));
+
+            //Scripts extras
+            bundles.Add(new ScriptBundle("~/bundles/extras").Include(
+                        "~/Content/lib/sc-date-time/dist/sc-date-time.js",
+                        "~/Content/lib/ui-router-extras/release/ct-ui-router-extras.min.js"
+                    ));
+
+            //Scripts módulos angular
+            bundles.Add(new ScriptBundle("~/bundles/modules").Include(
+                        "~/Content/app/app.js"
+                    ));
         }
     }
 }
