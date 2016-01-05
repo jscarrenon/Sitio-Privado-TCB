241c452aa03f769eb96090d0ad9edd4920345db8
Nicolás Torres <ntorres@kunder.cl>
2016-01-05 12:01:17 -0300

    Librerías y Helpers para AD B2C

--- Sitio Privado/App_Start/Startup.Auth.cs
+++ Sitio Privado/App_Start/Startup.Auth.cs
@@ -1,68 +1,96 @@
 ﻿using System;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using Sitio_Privado.Models;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using Sitio_Privado.Policies;
+using System.Threading;
+using System.Globalization;
 
 namespace Sitio_Privado
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        // The ACR claim is used to indicate which policy was executed
+        public const string AcrClaimType = "http://schemas.microsoft.com/claims/authnclassreference";
+        public const string PolicyKey = "b2cpolicy";
+        public const string OIDCMetadataSuffix = "/.well-known/openid-configuration";
+
+        // App config settings
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AadInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+
+        // B2C policy identifiers
+        public static string SignUpPolicyId = ConfigurationManager.AppSettings["ida:SignUpPolicyId"];
+        public static string SignInPolicyId = ConfigurationManager.AppSettings["ida:SignInPolicyId"];
+        public static string ProfilePolicyId = ConfigurationManager.AppSettings["ida:UserProfilePolicyId"];
+
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            OpenIdConnectAuthenticationOptions options = new OpenIdConnectAuthenticationOptions
             {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
+                // These are standard OpenID Connect parameters, with values pulled from web.config
+                ClientId = clientId,
+                RedirectUri = redirectUri,
+                PostLogoutRedirectUri = redirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications
                 {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });            
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+                    AuthenticationFailed = AuthenticationFailed,
+                    RedirectToIdentityProvider = OnRedirectToIdentityProvider,
+                },
+                Scope = "openid",
+                ResponseType = "id_token",
 
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+                // The PolicyConfigurationManager takes care of getting the correct Azure AD authentication
+                // endpoints from the OpenID Connect metadata endpoint.  It is included in the PolicyAuthHelpers folder.
+                ConfigurationManager = new PolicyConfigurationManager(
+                    String.Format(CultureInfo.InvariantCulture, aadInstance, tenant, "/v2.0", OIDCMetadataSuffix),
+                    new string[] { SignInPolicyId }),
 
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+                // This piece is optional - it is used for displaying the user's name in the navigation bar.
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                },
+            };
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            app.UseOpenIdConnectAuthentication(options);
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+        }
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+        // This notification can be used to manipulate the OIDC request before it is sent.  Here we use it to send the correct policy.
+        private async Task OnRedirectToIdentityProvider(RedirectToIdentityProviderNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            PolicyConfigurationManager mgr = notification.Options.ConfigurationManager as PolicyConfigurationManager;
+            if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+            {
+                OpenIdConnectConfiguration config = await mgr.GetConfigurationByPolicyAsync(CancellationToken.None, notification.OwinContext.Authentication.AuthenticationResponseRevoke.Properties.Dictionary[Startup.PolicyKey]);
+                notification.ProtocolMessage.IssuerAddress = config.EndSessionEndpoint;
+            }
+            else
+            {
+                OpenIdConnectConfiguration config = await mgr.GetConfigurationByPolicyAsync(CancellationToken.None, notification.OwinContext.Authentication.AuthenticationResponseChallenge.Properties.Dictionary[Startup.PolicyKey]);
+                notification.ProtocolMessage.IssuerAddress = config.AuthorizationEndpoint;
+            }
+        }
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+        // Used for avoiding yellow-screen-of-death
+        private Task AuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+            notification.Response.Redirect("/Home/Error?message=" + notification.Exception.Message);
+            return Task.FromResult(0);
         }
     }
 }
