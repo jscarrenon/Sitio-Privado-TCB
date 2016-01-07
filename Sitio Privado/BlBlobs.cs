8a2e414c5d02b3fec2ffdb3c6a433af4ab0939be
Guillermo Valenzuela <gevalenz@gmail.com>
2016-01-07 17:12:33 -0300

    Azure storage connection example

---
+++ Sitio Privado/BlBlobs.cs
@@ -0,0 +1,55 @@
+﻿using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.Web;
+
+
+namespace Sitio_Privado
+{
+    public class BlBlobs
+    {
+        public CloudBlobContainer GetBLOBRef()
+        {
+            String aux = ConfigurationManager.ConnectionStrings["StorageConnection"].ConnectionString;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(aux);
+
+            CloudBlobClient blobclient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer blobcontainer = blobclient.GetContainerReference("myvideos");
+            if (blobcontainer.CreateIfNotExists())
+                blobcontainer.SetPermissions(new BlobContainerPermissions
+                { PublicAccess = BlobContainerPublicAccessType.Blob });
+            return blobcontainer;
+        }
+        public List<string> GetBlobList()
+        {
+            CloudBlobContainer blobcontainer = GetBLOBRef();
+            List<string> blobs = new List<string>();
+            foreach (var item in blobcontainer.ListBlobs())
+            {
+                blobs.Add(item.Uri.ToString());
+            }
+            return blobs;
+        }
+        public void AddBlob(HttpPostedFileBase pic)
+        {
+            if (pic.ContentLength > 0)
+            {
+                CloudBlobContainer blobcontainer = GetBLOBRef();
+                CloudBlockBlob blob = blobcontainer.GetBlockBlobReference(pic.FileName);
+                blob.UploadFromStream(pic.InputStream);
+            }
+        }
+        internal void DeleteBlob(string name)
+        {
+            Uri ur = new Uri(name);
+            string fname = Path.GetFileName(ur.LocalPath);
+            CloudBlobContainer blobcontainer = GetBLOBRef();
+            CloudBlockBlob blob = blobcontainer.GetBlockBlobReference(fname);
+            blob.Delete();
+        }
+    }
+}
---
+++ Sitio Privado/Controllers/BlobsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitio_Privado.Controllers
+{
+    public class BlobsController : Controller
+    {
+        BlBlobs objbl = new BlBlobs();
+        // GET: Blobs
+        public ActionResult Index()
+        {
+            return View(objbl.GetBlobList());
+        }
+        [HttpPost]
+        public ActionResult Add(HttpPostedFileBase pic)
+        {
+            objbl.AddBlob(pic);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public string Remove(string name)
+        {
+            objbl.DeleteBlob(name);
+            return "Blob Removed Successfully";
+        }
+    }
+}
