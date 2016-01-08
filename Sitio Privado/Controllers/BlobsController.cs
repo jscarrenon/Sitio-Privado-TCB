4a4f2ff9ca32d74b65ca394242d7f77fb7e3c292
Guillermo Valenzuela <gevalenz@gmail.com>
2016-01-08 15:31:07 -0300

    Refactoring + Private files are downloaded using Shared Access Signatures

--- Sitio Privado/Controllers/BlobsController.cs
+++ Sitio Privado/Controllers/BlobsController.cs
@@ -11,6 +11,9 @@ namespace Sitio_Privado.Controllers
 {
     public class BlobsController : Controller
     {
+        private const string GetBlobAction = "GetBlob2";
+        private const string BlobsControllerName = "Blobs";
+
         private AzureStorageHelper azureStorageHelper = new AzureStorageHelper();
 
         // GET: Blobs
@@ -19,11 +22,10 @@ namespace Sitio_Privado.Controllers
             List<Blob> blobs = new List<Blob>();
             foreach (var item in azureStorageHelper.GetBlobsFromContainer(container)) {
                 CloudBlockBlob cbb = new CloudBlockBlob(new Uri(item.Uri.AbsoluteUri));
-                var uriString = Url.Action("GetBlob", "Blobs",
+                var uriString = Url.Action(GetBlobAction, BlobsControllerName,
                         routeValues: new { container = item.Container.Name, fileName = cbb.Name },
                         protocol: Request.Url.Scheme);
-                Blob blob = new Blob { Name = cbb.Name, //Url = item.StorageUri.PrimaryUri.ToString()
-                    Url = uriString };
+                Blob blob = new Blob { Name = cbb.Name, Url = uriString };
                 blobs.Add(blob);
             }
             return View(blobs);
@@ -37,6 +39,23 @@ namespace Sitio_Privado.Controllers
             Response.ContentType = blob.Properties.ContentType;
             blob.DownloadToStream(Response.OutputStream);
             return new EmptyResult();
+        }
+
+        public ActionResult GetBlob2(string container, string fileName) {
+            // Set the expiry time and permissions for the blob.
+            //In this case the start time is specified as a few minutes in the past, to mitigate clock skew.
+            //The shared access signature will be valid immediately.
+            var blob = azureStorageHelper.GetBlob(container, fileName);
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5);
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(5);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Read;
+
+            //Generate the shared access signature on the blob, setting the constraints directly on the signature.
+            string sasBlobToken = blob.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return Redirect(blob.Uri + sasBlobToken);
         }
     }
 }
