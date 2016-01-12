94e45198b85d6f48d2b1e8edb6085ded5fc4681d
Guillermo Valenzuela <gevalenz@gmail.com>
2016-01-12 16:57:42 -0300

    Refatoring params CreateUser action SyncApiController

--- Sitio Privado/Controllers/SyncApiController.cs
+++ Sitio Privado/Controllers/SyncApiController.cs
@@ -40,38 +40,59 @@ namespace Sitio_Privado.Controllers
         private static string SignInAlternativesParamKey = "alternativeSignInNamesInfo";
         #endregion
 
+        #region SyncAPIController Parameters
+        private static string NameParam = "names";
+        private static string SurnameParam = "surnames";
+        private static string RutParam = "rut";
+        private static string WorkAddressParam = "workAddress";
+        private static string HomeAddressParam = "homeAddress";
+        private static string CountryParam = "country";
+        private static string CityParam = "city";
+        private static string WorkPhoneParam = "workPhone";
+        private static string HomePhoneParam = "homePhone";
+        private static string EmailParam = "email";
+        private static string CheckingAccountParam = "checkingAccount";
+        private static string BankParam = "bank";
+        private static string TemporalPasswordParam = "temporalPassword";
+        #endregion
+
         private SyncApiClientHelper syncApiHelper = new SyncApiClientHelper();
 
         [HttpGet]
-        public async Task<HttpResponseMessage> GetUsers() {
+        public async Task<HttpResponseMessage> GetUsers()
+        {
             return await syncApiHelper.GetAllUsers(null);
         }
 
         [HttpPost]
-        public async Task<HttpResponseMessage> CreateUser(HttpRequestMessage request) {
-            dynamic content = JObject.Parse(await request.Content.ReadAsStringAsync());
+        public async Task<HttpResponseMessage> CreateUser(HttpRequestMessage request)
+        {
+            JObject content = JObject.Parse(await request.Content.ReadAsStringAsync());
             string json = GetCreateUserRequestBody(content);
-            //TODO: change response
+            //TODO: Change response
             return await syncApiHelper.CreateUser(json);
         }
 
         [HttpPatch]
-        public async Task<HttpResponseMessage> UpdateUser(string id, HttpRequestMessage request) {
+        public async Task<HttpResponseMessage> UpdateUser(string id, HttpRequestMessage request)
+        {
             HttpResponseMessage getUserResponse = await syncApiHelper.GetUserByRut(id);
             dynamic userResponse = JObject.Parse(await getUserResponse.Content.ReadAsStringAsync()).GetValue("value").ElementAt(0);
             dynamic content = JObject.Parse(await request.Content.ReadAsStringAsync());
             string json = GetUpdateUserRequestBody(content);
+            //TODO: Change response
             return await syncApiHelper.UpdateUser(userResponse.objectId.ToString(), json);
         }
 
         private string GetUpdateUserRequestBody(dynamic content)
         {
+            //TODO: update
             JObject json = new JObject();
             json.Add(GivenNameParamKey, "Guille");
             return json.ToString();
         }
 
-        private string GetCreateUserRequestBody(dynamic content)
+        private string GetCreateUserRequestBody(JObject content)
         {
             JObject json = new JObject();
             //Fixed parameters
@@ -80,30 +101,31 @@ namespace Sitio_Privado.Controllers
             json.Add(PasswordPoliciesParamKey, "DisablePasswordExpiration");
 
             //General information
-            json.Add(GivenNameParamKey, content.names);
-            json.Add(SurnameParamKey, content.surnames);
-            json.Add(RutParamKey, content.rut);
-            json.Add(WorkAddressParamKey, content.workAddress);
-            json.Add(HomeAddressParamKey, content.homeAddress);
-            json.Add(CountryParamKey, content.country);
-            json.Add(CityParamKey, content.city);
-            json.Add(WorkPhoneParamKey, content.workPhone);
-            json.Add(HomePhoneParamKey, content.homePhone);
-            json.Add(EmailParamKey, content.email);
-            json.Add(CheckingAccountParamKey, content.checkingAccount);
-            json.Add(BankParamKey, content.bank);
-            json.Add(DisplayNameParamKey, content.names + " " + content.surnames);
+            json.Add(GivenNameParamKey, content.GetValue(NameParam).ToString());
+            json.Add(SurnameParamKey, content.GetValue(SurnameParam).ToString());
+            json.Add(RutParamKey, content.GetValue(RutParam).ToString());
+            json.Add(WorkAddressParamKey, content.GetValue(WorkAddressParam).ToString());
+            json.Add(HomeAddressParamKey, content.GetValue(HomeAddressParam).ToString());
+            json.Add(CountryParamKey, content.GetValue(CountryParam).ToString());
+            json.Add(CityParamKey, content.GetValue(CityParam).ToString());
+            json.Add(WorkPhoneParamKey, content.GetValue(WorkPhoneParam).ToString());
+            json.Add(HomePhoneParamKey, content.GetValue(HomePhoneParam).ToString());
+            json.Add(EmailParamKey, content.GetValue(EmailParam).ToString());
+            json.Add(CheckingAccountParamKey, content.GetValue(CheckingAccountParam).ToString());
+            json.Add(BankParamKey, content.GetValue(BankParam).ToString());
+            json.Add(DisplayNameParamKey, content.GetValue(NameParam).ToString() + " " + 
+                content.GetValue(SurnameParam).ToString());
             
             //Temporal password
             JObject passwordProfile = new JObject();
-            passwordProfile.Add(PasswordParamKey, content.temporalPassword);
+            passwordProfile.Add(PasswordParamKey, content.GetValue(TemporalPasswordParam).ToString());
             passwordProfile.Add(ForcePasswordChangeParamKey, true);
             json.Add(PasswordProfileParamKey, passwordProfile);
 
             //Rut as login identifier
             JObject signInAlternative = new JObject();
             signInAlternative.Add(SignInTypeParamKey, "userName");
-            signInAlternative.Add(SignInValueParamKey, content.rut);
+            signInAlternative.Add(SignInValueParamKey, content.GetValue(RutParam).ToString());
             JArray signInAlternativesArray = new JArray(signInAlternative);
             json.Add(SignInAlternativesParamKey, signInAlternativesArray);
 
