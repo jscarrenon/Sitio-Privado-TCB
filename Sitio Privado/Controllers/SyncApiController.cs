51bb2346dea6da8c53893446a3124a4e8d61b1dd
Guillermo Valenzuela <gevalenz@gmail.com>
2016-01-12 16:04:25 -0300

    Refactoring

--- Sitio Privado/Controllers/SyncApiController.cs
+++ Sitio Privado/Controllers/SyncApiController.cs
@@ -14,13 +14,33 @@ namespace Sitio_Privado.Controllers
     //TODO: include authentication
     public class SyncApiController : ApiController
     {
+        #region Graph API Parameters
+        private static string AccountEnabledParamKey = "accountEnabled";
+        private static string CreationTypeParamKey = "creationType";
+        private static string PasswordPoliciesParamKey = "passwordPolicies";
+        private static string GivenNameParamKey = "givenName";
+        private static string SurnameParamKey = "surname";
+        private static string RutParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "RUT";
+        private static string WorkAddressParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "WorkAddress";
+        private static string HomeAddressParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "HomeAddress";
+        private static string CountryParamKey = "country";
+        private static string CityParamKey = "city";
+        private static string WorkPhoneParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "WorkPhoneNumber";
+        private static string HomePhoneParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "HomePhoneNumber";
+        private static string EmailParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "Email";
+        private static string CheckingAccountParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "CheckingAccount";
+        private static string BankParamKey = ConfigurationManager.AppSettings["b2c:Extensions"] + "Bank";
+        private static string DisplayNameParamKey = "displayName";
+        private static string PasswordParamKey = "password";
+        private static string ForcePasswordChangeParamKey = "forceChangePasswordNextLogin";
+        private static string PasswordProfileParamKey = "passwordProfile";
+        private static string SignInTypeParamKey = "type";
+        private static string SignInValueParamKey = "value";
+        private static string SignInAlternativesParamKey = "alternativeSignInNamesInfo";
+        #endregion
+
         private SyncApiClientHelper syncApiHelper = new SyncApiClientHelper();
         private static string ExtensionsPrefixe = ConfigurationManager.AppSettings["b2c:Extensions"];
-
-        [HttpGet]
-        public string Action() {
-            return "action";
-        }
 
         [HttpGet]
         public async Task<HttpResponseMessage> GetUsers() {
@@ -47,7 +67,7 @@ namespace Sitio_Privado.Controllers
         private string GetUpdateUserRequestBody(dynamic content)
         {
             JObject json = new JObject();
-            json.Add("givenName", "Guille");
+            json.Add(GivenNameParamKey, "Guille");
             return json.ToString();
         }
 
@@ -55,37 +75,37 @@ namespace Sitio_Privado.Controllers
         {
             JObject json = new JObject();
             //Fixed parameters
-            json.Add("accountEnabled", true);
-            json.Add("creationType", "NameCoexistence");
-            json.Add("passwordPolicies", "DisablePasswordExpiration");
+            json.Add(AccountEnabledParamKey, true);
+            json.Add(CreationTypeParamKey, "NameCoexistence");
+            json.Add(PasswordPoliciesParamKey, "DisablePasswordExpiration");
 
             //General information
-            json.Add("givenName", content.names);
-            json.Add("surname", content.surnames);
-            json.Add(ExtensionsPrefixe + "RUT", content.rut);
-            json.Add("streetAddress", content.workAddress);
-            json.Add(ExtensionsPrefixe + "HomeAddress", content.homeAddress);
-            json.Add("country", content.country);
-            json.Add("city", content.city);
-            json.Add("telephoneNumber", content.workPhone);
-            json.Add("mobile", content.mobilePhone);
-            json.Add(ExtensionsPrefixe + "Email", content.email);
-            json.Add(ExtensionsPrefixe + "CheckingAccount", content.checkingAccount);
-            json.Add(ExtensionsPrefixe + "Bank", content.bank);
-            json.Add("displayName", content.names + " " + content.surnames);
+            json.Add(GivenNameParamKey, content.names);
+            json.Add(SurnameParamKey, content.surnames);
+            json.Add(RutParamKey, content.rut);
+            json.Add(WorkAddressParamKey, content.workAddress);
+            json.Add(HomeAddressParamKey, content.homeAddress);
+            json.Add(CountryParamKey, content.country);
+            json.Add(CityParamKey, content.city);
+            json.Add(WorkPhoneParamKey, content.workPhone);
+            json.Add(HomePhoneParamKey, content.homePhone);
+            json.Add(EmailParamKey, content.email);
+            json.Add(CheckingAccountParamKey, content.checkingAccount);
+            json.Add(BankParamKey, content.bank);
+            json.Add(DisplayNameParamKey, content.names + " " + content.surnames);
             
             //Temporal password
             JObject passwordProfile = new JObject();
-            passwordProfile.Add("password", content.temporalPassword);
-            passwordProfile.Add("forceChangePasswordNextLogin", true);
-            json.Add("passwordProfile", passwordProfile);
+            passwordProfile.Add(PasswordParamKey, content.temporalPassword);
+            passwordProfile.Add(ForcePasswordChangeParamKey, true);
+            json.Add(PasswordProfileParamKey, passwordProfile);
 
             //Rut as login identifier
             JObject signInAlternative = new JObject();
-            signInAlternative.Add("type", "userName");
-            signInAlternative.Add("value", content.rut);
+            signInAlternative.Add(SignInTypeParamKey, "userName");
+            signInAlternative.Add(SignInValueParamKey, content.rut);
             JArray signInAlternativesArray = new JArray(signInAlternative);
-            json.Add("alternativeSignInNamesInfo", signInAlternativesArray);
+            json.Add(SignInAlternativesParamKey, signInAlternativesArray);
 
             return json.ToString();
         }
