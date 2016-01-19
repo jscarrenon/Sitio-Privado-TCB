382601e428af42356d2daf4c3853369bf0c7d7f7
Nicolás Torres <ntorres@kunder.cl>
2016-01-19 15:02:43 -0300

    Servicio para obtener usuario actual
    	modified:   Sitio Privado/Sitio Privado.csproj
    	modified:   Sitio Privado/Views/Home/Index.cshtml
    	modified:   Sitio Privado/Views/Shared/_LoginPartial.cshtml
    	modified:   Sitio Privado/app/common/controllers/bodyCtrl.ts
    	new file:   Sitio Privado/app/common/services/authService.ts
    	modified:   Sitio Privado/app/domain/Usuario.ts
    	modified:   Sitio Privado/app/home/homeCtrl.ts
    	modified:   Sitio Privado/app/home/index.html
    	modified:   Sitio Privado/app/home/mis-datos.html

---
+++ Sitio Privado/app/common/services/authService.ts
@@ -0,0 +1,34 @@
+﻿module app.common.services {
+
+    interface IAuth {
+        autenticado: boolean;
+        usuario: app.domain.IUsuario;
+    }
+
+    export class AuthService implements IAuth {
+        autenticado: boolean;
+        usuario: app.domain.IUsuario;
+                
+        static $inject = ['constantService', 'dataService'];
+        constructor(private constantService: ConstantService,
+            private dataService: DataService) {
+            this.getUsuarioActual();
+        }
+
+        getUsuarioActual(): void {
+            console.log("getUsuarioActual");
+            this.dataService.getSingle(this.constantService.mvcHomeURI + 'GetUsuarioActual').then((result: app.domain.IUsuario) => {
+                this.usuario = result;
+                if (this.usuario.Autenticado) {
+                    this.autenticado = true;
+                }
+                else {
+                    this.autenticado = false;
+                }
+            });
+        }
+    }
+
+    angular.module('tannerPrivadoApp')
+        .service('authService', AuthService);
+}
--- Sitio Privado/app/home/homeCtrl.ts
+++ Sitio Privado/app/home/homeCtrl.ts
@@ -1,23 +1,13 @@
 ﻿module app.home {
 
     interface IHomeViewModel {
-        usuario: app.domain.IUsuario;
     }
 
     class HomeCtrl implements IHomeViewModel {
 
-        usuario: app.domain.IUsuario;
-
         static $inject = ['constantService', 'dataService'];
         constructor(private constantService: app.common.services.ConstantService,
             private dataService: app.common.services.DataService) {
-            this.getUsuarioActual();
-        }
-
-        getUsuarioActual(): void {
-            this.dataService.getSingle(this.constantService.mvcHomeURI + 'GetUsuarioActual').then((result: app.domain.IUsuario) => {
-                this.usuario = result;
-            });
         }
     }
     angular.module('tannerPrivadoApp')
