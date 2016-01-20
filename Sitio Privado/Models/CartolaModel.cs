2f92b1474f244a1e9ba2840e86a63343ab1cf893
Nicolás Torres <ntorres@kunder.cl>
2016-01-20 16:27:01 -0300

    Modelo para Cartola (Inversiones Nacionales)
    	new file:   Sitio Privado/Models/CartolaModel.cs
    	new file:   Sitio Privado/app/domain/Cartola.ts
    	new file:   Sitio Privado/app/domain/CartolaInput.ts

---
+++ Sitio Privado/Models/CartolaModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitio_Privado.CartolaResumida;
+
+namespace Sitio_Privado.Models
+{
+    public class CartolaInput
+    {
+        public string _rut { get; set; }
+        public short _secuencia { get; set; }
+    }
+
+    public class Cartola
+    {
+        public string Rut { get; set; }
+        public string Periodo { get; set; }
+        public double SaldoCaja { get; set; }
+        public double TotalRentaFija { get; set; }
+        public double InstrumentosRentaFija { get; set; }
+        public double FondosMutuosRentaFija { get; set; }
+        public double TotalRentaVariable { get; set; }
+        public double AccionesNacionales { get; set; }
+        public double FondosMutuosRentaVariable { get; set; }
+        public double ForwardCompra { get; set; }
+        public double ForwardVenta { get; set; }
+        public double TotalInversiones { get; set; }
+
+        public Cartola() { }
+
+        public Cartola(CartolaInput input)
+        {
+            tann_cartola_resumida webService = new tann_cartola_resumida();
+            _cartola cartola = webService.cns_total_general(input._rut, input._secuencia);
+            Rut = cartola._rut;
+            Periodo = cartola._periodo;
+            SaldoCaja = cartola._saldocaja;
+            TotalRentaFija = cartola._totalrentafija;
+            InstrumentosRentaFija = cartola._instrf;
+            FondosMutuosRentaFija = cartola._ffmmrf;
+            TotalRentaVariable = cartola._totalrentavari;
+            AccionesNacionales = cartola._accnacion;
+            FondosMutuosRentaVariable = cartola._ffmmrv;
+            ForwardCompra = cartola._fwdcompra;
+            ForwardVenta = cartola._fwdventa;
+            TotalInversiones = cartola._totinversiones;
+        }
+    }
+}
---
+++ Sitio Privado/app/domain/Cartola.ts
@@ -0,0 +1,47 @@
+﻿module app.domain {
+    export interface ICartola {
+        Rut: string;
+        Periodo: string;
+        SaldoCaja: number;
+        TotalRentaFija: number;
+        InstrumentosRentaFija: number;
+        FondosMutuosRentaFija: number;
+        TotalRentaVariable: number;
+        AccionesNacionales: number;
+        FondosMutuosRentaVariable: number;
+        ForwardCompra: number;
+        ForwardVenta: number;
+        TotalInversiones: number;
+    }
+
+    export class Cartola extends app.domain.EntityBase implements ICartola {
+        constructor(public Rut: string,
+            public Periodo: string,
+            public SaldoCaja: number,
+            public TotalRentaFija: number,
+            public InstrumentosRentaFija: number,
+            public FondosMutuosRentaFija: number,
+            public TotalRentaVariable: number,
+            public AccionesNacionales: number,
+            public FondosMutuosRentaVariable: number,
+            public ForwardCompra: number,
+            public ForwardVenta: number,
+            public TotalInversiones: number) {
+
+            super();
+
+            this.Rut = Rut;
+            this.Periodo = Periodo;
+            this.SaldoCaja = SaldoCaja;
+            this.TotalRentaFija = TotalRentaFija;
+            this.InstrumentosRentaFija = InstrumentosRentaFija;
+            this.FondosMutuosRentaFija = FondosMutuosRentaFija;
+            this.TotalRentaVariable = TotalRentaVariable;
+            this.AccionesNacionales = AccionesNacionales;
+            this.FondosMutuosRentaVariable = FondosMutuosRentaVariable;
+            this.ForwardCompra = ForwardCompra;
+            this.ForwardVenta = ForwardVenta;
+            this.TotalInversiones = TotalInversiones;
+        }
+    }
+}
