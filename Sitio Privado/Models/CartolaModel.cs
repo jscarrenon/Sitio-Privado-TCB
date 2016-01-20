a066449f406db85313176f74385ac40d48050385
Nicolás Torres <ntorres@kunder.cl>
2016-01-20 17:32:18 -0300

    Porcentajes de cartola
    	modified:   Sitio Privado/Models/CartolaModel.cs
    	modified:   Sitio Privado/app/domain/Cartola.ts
    	modified:   Sitio Privado/app/mis-inversiones/nacionales.html

--- Sitio Privado/Models/CartolaModel.cs
+++ Sitio Privado/Models/CartolaModel.cs
@@ -17,15 +17,25 @@ namespace Sitio_Privado.Models
         public string Rut { get; set; }
         public string Periodo { get; set; }
         public double SaldoCaja { get; set; }
+        public double SaldoCajaPorcentaje { get; set; }
         public double TotalRentaFija { get; set; }
+        public double TotalRentaFijaPorcentaje { get; set; }
         public double InstrumentosRentaFija { get; set; }
+        public double InstrumentosRentaFijaPorcentaje { get; set; }
         public double FondosMutuosRentaFija { get; set; }
+        public double FondosMutuosRentaFijaPorcentaje { get; set; }
         public double TotalRentaVariable { get; set; }
+        public double TotalRentaVariablePorcentaje { get; set; }
         public double AccionesNacionales { get; set; }
+        public double AccionesNacionalesPorcentaje { get; set; }
         public double FondosMutuosRentaVariable { get; set; }
+        public double FondosMutuosRentaVariablePorcentaje { get; set; }
         public double ForwardCompra { get; set; }
+        public double ForwardCompraPorcentaje { get; set; }
         public double ForwardVenta { get; set; }
+        public double ForwardVentaPorncentaje { get; set; }
         public double TotalInversiones { get; set; }
+        public double TotalInversionesPorcentaje { get; set; }
 
         public Cartola() { }
 
@@ -35,6 +45,7 @@ namespace Sitio_Privado.Models
             _cartola cartola = webService.cns_total_general(input._rut, input._secuencia);
             Rut = cartola._rut;
             Periodo = cartola._periodo;
+
             SaldoCaja = cartola._saldocaja;
             TotalRentaFija = cartola._totalrentafija;
             InstrumentosRentaFija = cartola._instrf;
@@ -45,6 +56,30 @@ namespace Sitio_Privado.Models
             ForwardCompra = cartola._fwdcompra;
             ForwardVenta = cartola._fwdventa;
             TotalInversiones = cartola._totinversiones;
+
+            SaldoCajaPorcentaje = GetPorcentaje(SaldoCaja, TotalInversiones);
+            TotalRentaFijaPorcentaje = GetPorcentaje(TotalRentaFija, TotalInversiones);
+            InstrumentosRentaFijaPorcentaje = GetPorcentaje(InstrumentosRentaFija, TotalInversiones);
+            FondosMutuosRentaFijaPorcentaje = GetPorcentaje(FondosMutuosRentaFija, TotalInversiones);
+            TotalRentaVariablePorcentaje = GetPorcentaje(TotalRentaVariable, TotalInversiones);
+            AccionesNacionalesPorcentaje = GetPorcentaje(AccionesNacionales, TotalInversiones);
+            FondosMutuosRentaVariablePorcentaje = GetPorcentaje(FondosMutuosRentaVariable, TotalInversiones);
+            ForwardCompraPorcentaje = GetPorcentaje(ForwardCompra, TotalInversiones);
+            ForwardVentaPorncentaje = GetPorcentaje(ForwardVenta, TotalInversiones);
+            TotalInversionesPorcentaje = GetPorcentaje(TotalInversiones, TotalInversiones);
+        }
+
+        private static double GetPorcentaje(double monto, double total)
+        {
+            double resultado = 0;
+
+            if(total != 0)
+            {
+                resultado = monto / total;
+                resultado = resultado * 100;
+            }
+
+            return resultado;
         }
     }
 }
--- Sitio Privado/app/domain/Cartola.ts
+++ Sitio Privado/app/domain/Cartola.ts
@@ -3,45 +3,75 @@
         Rut: string;
         Periodo: string;
         SaldoCaja: number;
+        SaldoCajaPorcentaje: number;
         TotalRentaFija: number;
+        TotalRentaFijaPorcentaje: number;
         InstrumentosRentaFija: number;
+        InstrumentosRentaFijaPorcentaje: number;
         FondosMutuosRentaFija: number;
+        FondosMutuosRentaFijaPorcentaje: number;
         TotalRentaVariable: number;
+        TotalRentaVariablePorcentaje: number;
         AccionesNacionales: number;
+        AccionesNacionalesPorcentaje: number;
         FondosMutuosRentaVariable: number;
+        FondosMutuosRentaVariablePorcentaje: number;
         ForwardCompra: number;
+        ForwardCompraPorcentaje;
         ForwardVenta: number;
+        ForwardVentaPorcentaje: number;
         TotalInversiones: number;
+        TotalInversionesPorcentaje: number;
     }
 
     export class Cartola extends app.domain.EntityBase implements ICartola {
         constructor(public Rut: string,
             public Periodo: string,
             public SaldoCaja: number,
+            public SaldoCajaPorcentaje: number,
             public TotalRentaFija: number,
+            public TotalRentaFijaPorcentaje: number,
             public InstrumentosRentaFija: number,
+            public InstrumentosRentaFijaPorcentaje: number,
             public FondosMutuosRentaFija: number,
+            public FondosMutuosRentaFijaPorcentaje: number,
             public TotalRentaVariable: number,
+            public TotalRentaVariablePorcentaje: number,
             public AccionesNacionales: number,
+            public AccionesNacionalesPorcentaje: number,
             public FondosMutuosRentaVariable: number,
+            public FondosMutuosRentaVariablePorcentaje: number,
             public ForwardCompra: number,
+            public ForwardCompraPorcentaje: number,
             public ForwardVenta: number,
-            public TotalInversiones: number) {
+            public ForwardVentaPorcentaje: number,
+            public TotalInversiones: number,
+            public TotalInversionesPorcentaje: number) {
 
             super();
 
             this.Rut = Rut;
             this.Periodo = Periodo;
             this.SaldoCaja = SaldoCaja;
+            this.SaldoCajaPorcentaje = SaldoCajaPorcentaje;
             this.TotalRentaFija = TotalRentaFija;
+            this.TotalRentaFijaPorcentaje = TotalRentaFijaPorcentaje;
             this.InstrumentosRentaFija = InstrumentosRentaFija;
+            this.InstrumentosRentaFijaPorcentaje = InstrumentosRentaFijaPorcentaje;
             this.FondosMutuosRentaFija = FondosMutuosRentaFija;
+            this.FondosMutuosRentaFijaPorcentaje = FondosMutuosRentaFijaPorcentaje;
             this.TotalRentaVariable = TotalRentaVariable;
+            this.TotalRentaVariablePorcentaje = TotalRentaVariablePorcentaje;
             this.AccionesNacionales = AccionesNacionales;
+            this.AccionesNacionalesPorcentaje = AccionesNacionalesPorcentaje;
             this.FondosMutuosRentaVariable = FondosMutuosRentaVariable;
+            this.FondosMutuosRentaVariablePorcentaje = FondosMutuosRentaVariablePorcentaje;
             this.ForwardCompra = ForwardCompra;
+            this.ForwardCompraPorcentaje = ForwardCompraPorcentaje;
             this.ForwardVenta = ForwardVenta;
+            this.ForwardVentaPorcentaje = ForwardVentaPorcentaje;
             this.TotalInversiones = TotalInversiones;
+            this.TotalInversionesPorcentaje = TotalInversionesPorcentaje;
         }
     }
 }
