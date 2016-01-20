9aab404c10ab331e2277394c8c82650353aad942
Nicolás Torres <ntorres@kunder.cl>
2016-01-20 11:33:24 -0300

    Se agrega campo opcional en ruta de mis-inversiones (seccion) para manejar acceso vía routeParams
    	modified:   Sitio Privado/app/app.ts
    	modified:   Sitio Privado/app/home/index.html
    	modified:   Sitio Privado/app/mis-inversiones/misInversionesCtrl.ts
    	modified:   Sitio Privado/app/mis-inversiones/misInversionesDocumentosCtrl.ts

--- Sitio Privado/app/mis-inversiones/misInversionesDocumentosCtrl.ts
+++ Sitio Privado/app/mis-inversiones/misInversionesDocumentosCtrl.ts
@@ -2,6 +2,18 @@
 
     class MisInversionesDocumentosCtrl extends MisInversionesCtrl {
 
+        static $inject = ['constantService', 'dataService', '$routeParams'];
+        constructor(constantService: app.common.services.ConstantService,
+            dataService: app.common.services.DataService,
+            $routeParams: app.misInversiones.IMisInversionesRouteParams) {
+
+            super(constantService, dataService, $routeParams);
+
+            this.setTemplates();
+            this.seccionId = 0;
+            this.seleccionarSeccion(this.seccionId);
+        }
+
         setTemplates(): void {
             this.templates = [];
             this.templates[0] = "estado-documentos_pendientes.html";
