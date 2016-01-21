8df0fbf0c998d321877d5449efdb8f8998bb72f0
Nicolás Torres <ntorres@kunder.cl>
2016-01-21 10:26:41 -0300

    Modelo de Documento y Controlador de listas de pendientes y firmados
    	new file:   Sitio Privado/Controllers/DocumentoController.cs
    	new file:   Sitio Privado/Models/DocumentoModel.cs

---
+++ Sitio Privado/Controllers/DocumentoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sitio_Privado.Models;
+using Sitio_Privado.DocumentosPendientesFirma;
+
+namespace Sitio_Privado.Controllers
+{
+    public class DocumentoController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult GetListPendientes([FromBody]DocumentosPendientesInput input)
+        {
+            try
+            {
+                tann_documentos webService = new tann_documentos();
+                _operacion[] documentos = webService.cns_operacion_pendiente(input.rut);
+
+                List<Documento> lista = new List<Documento>();
+                foreach (_operacion documento in documentos)
+                {
+                    lista.Add(new Documento(documento));
+                }
+
+                return Ok(lista);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetListFirmados([FromBody]DocumentosFirmadosInput input)
+        {
+            try
+            {
+                tann_documentos webService = new tann_documentos();
+                _operacion[] documentos = webService.cns_operacion_firmada(input.rut, input.fechaIni, input.fechaFin)
+
+                List<Documento> lista = new List<Documento>();
+                foreach (_operacion documento in documentos)
+                {
+                    lista.Add(new Documento(documento));
+                }
+
+                return Ok(lista);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Sitio Privado/Models/DocumentoModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitio_Privado.DocumentosPendientesFirma;
+
+namespace Sitio_Privado.Models
+{
+    public class DocumentosPendientesInput
+    {
+        public string rut { get; set; }
+    }
+
+    public class DocumentosFirmadosInput
+    {
+        public string rut { get; set; }
+        public string fechaIni { get; set; }
+        public string fechaFin { get; set; }
+    }
+
+    public class Documento
+    {
+        public string Codigo { get; set; }
+        public string Producto { get; set; }
+        public string Tipo { get; set; }
+        public string Folio { get; set; }
+        public string FechaCreacion { get; set; }
+        public string Leido { get; set; }
+        public string Firmado { get; set; }
+        public string Ruta { get; set; }
+        public string Resultados { get; set; }
+
+        public Documento() { }
+
+        public Documento(_operacion documento)
+        {
+            Codigo = documento._code;
+            Producto = documento._producto;
+            Tipo = documento._tipo;
+            Folio = documento._folio;
+            FechaCreacion = documento._fechacre;
+            Leido = documento._leido;
+            Firmado = documento._firmado;
+            Ruta = documento._ruta;
+            Resultados = documento._results;
+        }
+    }
+}
