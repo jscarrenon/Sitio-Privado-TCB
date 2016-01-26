ebc8ad9bbcb683aacbe887c0f48bf9d4dc3464a9
Juan Ignacio Sánchez <jisanchez@kunder.cl>
2016-01-26 13:25:31 -0300

    Corrección en fecha y valores leido-firmado de documentos

--- Sitio Privado/Extras/Converters.cs
+++ Sitio Privado/Extras/Converters.cs
@@ -10,12 +10,12 @@ namespace Sitio_Privado.Extras
         /// <summary>
         /// Convierte string de fecha a tipo DateTime
         /// </summary>
-        /// <param name="fecha">Fecha en formato dd-mm-YYYY HH:mm:ss</param>
+        /// <param name="fecha">Fecha en formato dd-MM-yyyy HH:mm:ss</param>
         /// <returns>DateTime</returns>
         public static DateTime getFecha(string fecha)
         {
             fecha = fecha.Substring(0, 10);
-            return DateTime.ParseExact(fecha, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            return DateTime.ParseExact(fecha, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
         }
 
     }
