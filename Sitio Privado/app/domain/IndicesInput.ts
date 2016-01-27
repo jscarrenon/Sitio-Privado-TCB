27acb9601440e0db8ef79c249d67216531a167f3
Nicolás Torres <ntorres@kunder.cl>
2016-01-27 11:42:58 -0300

    Modelo typescript para Indices
    	new file:   Sitio Privado/app/domain/Indices.ts
    	new file:   Sitio Privado/app/domain/IndicesInput.ts

---
+++ Sitio Privado/app/domain/IndicesInput.ts
@@ -0,0 +1,14 @@
+﻿module app.domain {
+    export interface IIndicesInput {
+        xfecha: string;
+    }
+
+    export class IndicesInput extends app.domain.InputBase implements IIndicesInput {
+        constructor(public xfecha: string) {
+
+            super();
+
+            this.xfecha = xfecha;
+        }
+    }
+}
