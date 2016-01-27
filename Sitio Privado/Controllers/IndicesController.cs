b45ef68f9fbb6e687fcd626c186fef59b120a81d
Nicolás Torres <ntorres@kunder.cl>
2016-01-27 12:11:30 -0300

    Controlador api para Índices
    	new file:   Sitio Privado/Controllers/IndicesController.cs

---
+++ Sitio Privado/Controllers/IndicesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sitio_Privado.Models;
+
+namespace Sitio_Privado.Controllers
+{
+    public class IndicesController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult GetSingle([FromBody]IndicesInput input)
+        {
+            try
+            {
+                Indices indices = new Indices(input);
+                return Ok(indices);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
