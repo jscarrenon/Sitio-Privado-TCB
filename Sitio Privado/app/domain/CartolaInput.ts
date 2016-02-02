111d3d5f83953666aa989a6bc481ce8177788adb
Juan Ignacio Sánchez <jisanchez@kunder.cl>
2016-02-02 10:27:01 -0300

    Cambios en modelo de Cartola debido a modificación de ws

--- Sitio Privado/app/domain/CartolaInput.ts
+++ Sitio Privado/app/domain/CartolaInput.ts
@@ -1,17 +1,14 @@
 ﻿module app.domain {
     export interface ICartolaInput {
         _rut: string;
-        _secuencia: number;
     }
 
     export class CartolaInput extends app.domain.InputBase implements ICartolaInput {
-        constructor(public _rut: string,
-            public _secuencia: number) {
+        constructor(public _rut: string) {
 
             super();
 
             this._rut = _rut;
-            this._secuencia = _secuencia;
         }
     }
 }
