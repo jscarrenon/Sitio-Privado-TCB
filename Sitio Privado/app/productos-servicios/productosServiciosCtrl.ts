00918301ac9208be7b9ef5ef5ed3dd3e84df22e7
Guillermo Valenzuela <gevalenz@gmail.com>
2016-02-05 10:03:10 -0300

    Initial config for products and services

---
+++ Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
@@ -0,0 +1,11 @@
+﻿module app.productosServicios {
+
+    class ProductosServiciosCtrl {
+        static $inject = ['constantService', 'dataService'];
+        constructor(private constantService: app.common.services.ConstantService,
+            private dataService: app.common.services.DataService) {
+        }
+    }
+    angular.module('tannerPrivadoApp')
+        .controller('ProductosServiciosCtrl', ProductosServiciosCtrl);
+}
