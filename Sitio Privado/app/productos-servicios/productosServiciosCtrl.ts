3f697ce9bdf6bc2286faf14f282c0233ba721658
Guillermo Valenzuela <gevalenz@gmail.com>
2016-02-05 11:30:54 -0300

    Added list of all products + footer

--- Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
+++ Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
@@ -3,14 +3,16 @@
     interface IProductosServiciosViewModel {
         categoriaCliente: app.domain.ICategoria;
         categoriaClienteInput: app.domain.ICategoriaClienteInput;
-
+        productos: app.domain.IProducto[];
         getCategoriaCliente(input: app.domain.ICategoriaClienteInput): void;
+        getProductos(): void;
     }
 
     class ProductosServiciosCtrl implements IProductosServiciosViewModel {
         
         categoriaCliente: app.domain.ICategoria;
         categoriaClienteInput: app.domain.ICategoriaClienteInput;
+        productos: app.domain.IProducto[];
 
         static $inject = ['constantService', 'dataService', 'authService', 'extrasService'];
         constructor(private constantService: app.common.services.ConstantService,
@@ -26,6 +28,13 @@
             this.dataService.postWebService(this.constantService.apiCategoriaURI + 'getSingleCliente', input)
                 .then((result: app.domain.ICategoria) => {
                     this.categoriaCliente = result;
+                });
+        }
+
+        getProductos(): void {
+            this.dataService.get(this.constantService.apiProductoURI + 'getList')
+                .then((result: app.domain.IProducto[]) => {
+                    this.productos = result;
                 });
         }
     }
