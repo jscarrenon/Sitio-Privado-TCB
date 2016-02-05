86ed12e5b7d0d51fdd4f887f39ecec68a640fb7f
Guillermo Valenzuela <gevalenz@gmail.com>
2016-02-05 10:39:36 -0300

    Calling getClientCategory service

--- Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
+++ Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
@@ -1,9 +1,31 @@
 ﻿module app.productosServicios {
 
-    class ProductosServiciosCtrl {
-        static $inject = ['constantService', 'dataService'];
+    interface IProductosServiciosViewModel {
+        categoriaCliente: app.domain.ICategoria;
+        categoriaClienteInput: app.domain.ICategoriaClienteInput;
+        getCategoriaCliente(input: app.domain.ICategoriaClienteInput): void;
+    }
+
+    class ProductosServiciosCtrl implements IProductosServiciosViewModel {
+        
+        categoriaCliente: app.domain.ICategoria;
+        categoriaClienteInput: app.domain.ICategoriaClienteInput;
+
+        static $inject = ['constantService', 'dataService', 'authService', 'extrasService'];
         constructor(private constantService: app.common.services.ConstantService,
-            private dataService: app.common.services.DataService) {
+            private dataService: app.common.services.DataService,
+            private authService: app.common.services.AuthService,
+            private extrasService: app.common.services.ExtrasService) {
+
+            this.categoriaClienteInput = new app.domain.CategoriaClienteInput(parseInt(this.extrasService.getRutParteEntera(this.authService.usuario.Rut)));
+            this.getCategoriaCliente(this.categoriaClienteInput);
+        }
+
+        getCategoriaCliente(input: app.domain.ICategoriaClienteInput): void {
+            this.dataService.postWebService(this.constantService.apiCategoriaURI + 'getSingleCliente', input)
+                .then((result: app.domain.ICategoria) => {
+                    this.categoriaCliente = result;
+                });
         }
     }
     angular.module('tannerPrivadoApp')
