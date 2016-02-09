589741dc42da01bcee94a9fb8def431b9cacce1c
Guillermo Valenzuela <gevalenz@gmail.com>
2016-02-09 14:21:12 -0300

    Pre-production config + products images

--- Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
+++ Sitio Privado/app/productos-servicios/productosServiciosCtrl.ts
@@ -4,8 +4,10 @@
         categoriaCliente: app.domain.ICategoria;
         categoriaClienteInput: app.domain.ICategoriaClienteInput;
         productos: app.domain.IProducto[];
+        imagenProducto: {};
         getCategoriaCliente(input: app.domain.ICategoriaClienteInput): void;
         getProductos(): void;
+        scrollTo(id: string): void;
     }
 
     class ProductosServiciosCtrl implements IProductosServiciosViewModel {
@@ -13,6 +15,7 @@
         categoriaCliente: app.domain.ICategoria;
         categoriaClienteInput: app.domain.ICategoriaClienteInput;
         productos: app.domain.IProducto[];
+        imagenProducto: {};
 
         static $inject = ['constantService', 'dataService', 'authService', 'extrasService', '$anchorScroll'];
         constructor(private constantService: app.common.services.ConstantService,
@@ -21,6 +24,7 @@
             private extrasService: app.common.services.ExtrasService,
             private $anchorScroll: ng.IAnchorScrollService) {
 
+            this.setImagenes();
             this.categoriaClienteInput = new app.domain.CategoriaClienteInput(parseInt(this.extrasService.getRutParteEntera(this.authService.usuario.Rut)));
             this.getCategoriaCliente(this.categoriaClienteInput);
         }
@@ -46,6 +50,22 @@
 
         scrollTo(id: string): void {
             this.$anchorScroll(id);
+        }
+
+        setImagenes(): void {
+            this.imagenProducto = {};
+            this.imagenProducto["1"] = "moneda-ext";
+            this.imagenProducto["2"] = "irf";
+            this.imagenProducto["3"] = "distribucion-fm";
+            this.imagenProducto["5"] = "pactos";
+            this.imagenProducto["6"] = "operaciones-prestamo";
+            this.imagenProducto["7"] = "operaciones-simultaneas";
+            this.imagenProducto["8"] = "renta-fija-inter";
+            this.imagenProducto["9"] = "operaciones-simultaneas";
+            this.imagenProducto["10"] = "operaciones-prestamo";
+            this.imagenProducto["11"] = "moneda-ext";
+            this.imagenProducto["12"] = "renta-internacional";
+            this.imagenProducto["13"] = "admin-cartera";
         }
     }
     angular.module('tannerPrivadoApp')
