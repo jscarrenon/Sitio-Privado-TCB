94c2a6b49ed440965f20f1f9695e7811c646bffd
Nicolás Torres <ntorres@kunder.cl>
2016-02-09 10:42:40 -0300

    Directiva para despliegue de spinner (loading)
    	new file:   Sitio Privado/Resources/img/loader-1.gif
    	modified:   Sitio Privado/Sitio Privado.csproj
    	modified:   Sitio Privado/app/common/controllers/agenteCtrl.ts
    	new file:   Sitio Privado/app/common/directives/loadingDirective.ts
    	modified:   Sitio Privado/app/home/index.html
    	modified:   Sitio Privado/gulpfile.js

---
+++ Sitio Privado/app/common/directives/loadingDirective.ts
@@ -0,0 +1,43 @@
+﻿module app.common.directives {
+
+    interface ILoadingDirectiveScope extends ng.IScope {
+        param: boolean;
+        isLoading(): boolean;
+    }
+
+
+    class LoadingDirective implements ng.IDirective {
+        restrict = 'E';
+        template = '<div class="loading-spiner"><img src="../.build/img/loader-1.gif" /></div>';
+        replace = true;
+        scope = {
+            param: '='
+        }
+
+        link = (scope: ILoadingDirectiveScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+
+            scope.isLoading = function () {
+                return scope.param;
+            };
+
+            scope.$watch(scope.isLoading, function (v) {
+                if (v) {
+                    element.show();
+                } else {
+                    element.hide();
+                }
+            });
+
+        }
+
+        static factory(): ng.IDirectiveFactory {
+            const directive = () => new LoadingDirective();
+            return directive;
+        }
+    }
+
+    angular.module('tannerPrivadoApp')
+        .directive('loading', LoadingDirective.factory());
+
+}
+
