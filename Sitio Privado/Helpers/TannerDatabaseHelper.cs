e88bde2c7dc64cc7451e0ac8f8afd1bee1343e71
Guillermo Valenzuela <gevalenz@gmail.com>
2016-02-12 15:38:16 -0300

    Updates from database

--- Sitio Privado/Helpers/TannerDatabaseHelper.cs
+++ Sitio Privado/Helpers/TannerDatabaseHelper.cs
@@ -58,45 +58,18 @@ namespace Sitio_Privado.Helpers
                 if (reader[2] != null) user.RutVD = reader[2].ToString();
                 if (reader[3] != null) user.WorkAddress = reader[3].ToString();
                 if (reader[4] != null) user.HomeAddress = reader[4].ToString();
-                if (reader[5] != null) user.City = GetCityFromCode(reader[5].ToString());
-                if (reader[6] != null) user.Country = GetCountryFromCode(reader[6].ToString());
+                if (reader[5] != null) user.City = reader[5].ToString();
+                if (reader[6] != null) user.Country = reader[6].ToString();
                 if (reader[7] != null) user.WorkPhone = reader[7].ToString();
                 if (reader[8] != null) user.HomePhone = reader[8].ToString();
                 if (reader[9] != null) user.Email = reader[9].ToString();
                 if (reader[10] != null) user.CheckingAccount = reader[10].ToString();
                 if (reader[11] != null) { user.Bank = reader[11].ToString(); }
                 if (reader[12] != null) user.TemporalPassword = reader[12].ToString();
+                if (reader[13] != null) { string timestamp = reader[13].ToString(); }
                 userList.Add(user);
             }
             return userList;
-        }
-
-        private string GetCountryFromCode(string countryCode)
-        {
-            SqlCommand command = new SqlCommand("SELECT Nom_Pais from dbo.Tb_Pais WHERE Cod_Pais = @0", sqlConnection);
-            command.Parameters.Add(new SqlParameter("0", countryCode));
-            SqlDataReader reader = command.ExecuteReader();
-            if (reader.Read())
-            {
-                if(reader[0] != null)
-                    return reader[0].ToString();
-            }
-
-            return null;
-        }
-
-        private string GetCityFromCode(string cityCode)
-        {
-            SqlCommand command = new SqlCommand("SELECT Nom_Comuna from dbo.Tb_Comuna WHERE Cod_Comuna = @0", sqlConnection);
-            command.Parameters.Add(new SqlParameter("0", cityCode));
-            SqlDataReader reader = command.ExecuteReader();
-            if (reader.Read())
-            {
-                if (reader[0] != null)
-                    return reader[0].ToString();
-            }
-
-            return null;
         }
     }
 }
