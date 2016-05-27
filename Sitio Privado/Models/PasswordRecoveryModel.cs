27a8ea12bda063717c96846decdf979a0a70374e
Nicolás Torres <ntorres@kunder.cl>
2016-05-27 16:47:39 -0400

    Password recovery model

---
+++ Sitio Privado/Models/PasswordRecoveryModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sitio_Privado.Models
+{
+    public class PasswordRecoveryModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Rut { get; set; }
+    }
+}
