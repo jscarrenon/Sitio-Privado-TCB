151cd80af39d6d46f0cf6e4d31279c98b5b9ffe2
Nicolás Torres <ntorres@kunder.cl>
2016-05-31 11:12:09 -0400

    Change Password model

---
+++ Sitio Privado/Extras/PasswordFormatValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Sitio_Privado.Extras
+{
+    public class PasswordFormatValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string password = (string)value;
+
+            if (!(password != null && password.Any(char.IsUpper) && password.Any(char.IsLower)
+                && password.Any(char.IsDigit)))
+            {
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+            return null;
+        }
+    }
+}
---
+++ Sitio Privado/Models/ChangePasswordModel.cs
@@ -0,0 +1,25 @@
+﻿using Sitio_Privado.Extras;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sitio_Privado.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required]
+        [DisplayName("Contraseña actual")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "La contraseña debe tener entre {2} y {1} caracteres", MinimumLength = 8)]
+        [RegularExpression(@"^([a-zA-Z0-9]+)$", ErrorMessage = "La clave sólo puede contener letras mayúsculas, minúsculas y números")]
+        [PasswordFormatValidation(ErrorMessage = "La clave debe contener mayúsculas, minúsculas y números")]
+        [DisplayName("Nueva contraseña")]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Nueva contraseña (repetir)")]
+        [Compare("Password", ErrorMessage = "Las contraseñas ingresadas no coinciden")]
+        public string PasswordValidation { get; set; }
+    }
+}
