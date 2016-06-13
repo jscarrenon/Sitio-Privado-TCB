01ae062f6ba1c4d49c63656cf7094f3086131ae5
Nicolás Torres <ntorres@kunder.cl>
2016-06-13 09:10:26 -0400

    New password validation

--- Sitio Privado/Extras/PasswordFormatValidationAttribute.cs
+++ Sitio Privado/Extras/PasswordFormatValidationAttribute.cs
@@ -9,11 +9,30 @@ namespace Sitio_Privado.Extras
         {
             string password = (string)value;
 
-            if (!(password != null && password.Any(char.IsUpper) && password.Any(char.IsLower)
-                && password.Any(char.IsDigit)))
+            //Validations
+            bool[] validations = new bool[4];
+
+            //1: lowercase characters
+            bool validation1 = password.Any(char.IsLower);
+            validations[0] = validation1;
+
+            //2: uppercase characters
+            bool validation2 = password.Any(char.IsUpper);
+            validations[1] = validation2;
+
+            //3: numbers
+            bool validation3 = password.Any(char.IsDigit);
+            validations[2] = validation3;
+
+            //4: symbols
+            bool validation4 = password.IndexOfAny(@"@#$%^&*\-_!+=[\]{}\|\\:´,.?\/`~""();".ToCharArray()) != -1;
+            validations[3] = validation4;
+
+            if (!(password != null && validations.Where(v => v == true).Count() >= 3))
             {
                 return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
             }
+
             return null;
         }
     }
--- Sitio Privado/Models/ChangePasswordModel.cs
+++ Sitio Privado/Models/ChangePasswordModel.cs
@@ -12,9 +12,9 @@ namespace Sitio_Privado.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(16, ErrorMessage = "La contraseña debe tener entre {2} y {1} caracteres", MinimumLength = 8)]
-        [RegularExpression(@"^([a-zA-Z0-9]+)$", ErrorMessage = "La clave sólo puede contener letras mayúsculas, minúsculas y números")]
-        [PasswordFormatValidation(ErrorMessage = "La clave debe contener mayúsculas, minúsculas y números")]
+        [StringLength(16, ErrorMessage = "La contraseña debe tener entre {2} y {1} caracteres", MinimumLength = 6)]
+        [RegularExpression(@"^([a-zA-Z0-9@#$%^&*\-_!+=[\]{}\|\\:´,.?\/`~""();]+)$", ErrorMessage = "La clave sólo puede contener mayúsculas, minúsculas, números y símbolos especiales")]
+        [PasswordFormatValidation(ErrorMessage = "La clave debe contener mayúsculas, minúsculas, números y/o símbolos especiales")]
         [DisplayName("Nueva contraseña")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
