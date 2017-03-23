83a8acb6ac7d64692d7faa879aff3e396c09f9e8
José Ignacio Navarro <josencv@gmail.com>
2017-03-23 18:57:00 -0300

    Added logger

---
+++ Sitio Privado/Infraestructure/Logging/WebApiNLogExceptionLogger.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using Microsoft.ApplicationInsights;
+using NLog;
+
+namespace Sitio_Privado.Infrastructure.Logging
+{
+    /// <summary>
+    /// This class logs any WebApi unhandled exception into Serilog
+    /// </summary>
+    public class WebApiNLogExceptionLogger : IExceptionLogger
+    {
+        Logger logger = LogManager.GetLogger("SessionLog");
+
+        public Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            TelemetryClient telemetry = new TelemetryClient();
+            telemetry.TrackException(context.Exception);
+
+            logger.Error(context.Exception, string.Format("Unhandled endpoint exception calling: {0} {1}", context.Request.Method, context.Request.RequestUri));
+
+            return Task.FromResult(0);
+        }
+    }
+}
