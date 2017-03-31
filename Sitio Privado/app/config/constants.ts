e2e9cb46f892fb2a316e70c6abf4cdb9feca528e
José Ignacio Navarro <josencv@gmail.com>
2017-03-31 17:16:27 -0300

    Fixed oauth url

--- Sitio Privado/app/config/constants.ts
+++ Sitio Privado/app/config/constants.ts
@@ -4,7 +4,7 @@ module app.config {
         ENV: 'local',
         CLIENT_ID: 'passwordgrant',
         CLIENT_SECRET: 'secret',
-        OAUTH2_URL: 'https://oauthdesa.tanner.cl/auth',
+        OAUTH2_URL: 'https://oauthdesa.tanner.cl/',
         TANNER_PUBLIC_SITE_URL: 'http://www.kunder.cl/tannercl/'
     };
 }
