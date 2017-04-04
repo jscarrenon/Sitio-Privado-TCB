7e3f7866aac69952c70c00e91351649132f7b15d
José Ignacio Navarro <josencv@gmail.com>
2017-04-04 10:08:00 -0300

    Changed frontend client credentials

--- Sitio Privado/app/config/constants.ts
+++ Sitio Privado/app/config/constants.ts
@@ -1,9 +1,9 @@
 module app.config {
     // Kunder development and testing servers
     export const CONFIG = {
-        ENV: 'local',
-        CLIENT_ID: 'passwordgrant',
-        CLIENT_SECRET: 'secret',
+        ENV: 'debug',
+        CLIENT_ID: 'dev_customer_password',
+        CLIENT_SECRET: 'rdkbKVM7neKs15wX0YgOzQjTZ8JF4SRNYs-KAjNr',
         OAUTH2_URL: 'https://oauthdesa.tanner.cl/',
         TANNER_PUBLIC_SITE_URL: 'http://www.kunder.cl/tannercl/'
     };
--- Sitio Privado/config/Local/constants.ts
+++ Sitio Privado/config/Local/constants.ts
@@ -2,8 +2,8 @@ module app.config {
     // Kunder development and testing servers
     export const CONFIG = {
         ENV: 'local',
-        CLIENT_ID: 'passwordgrant',
-        CLIENT_SECRET: 'secret',
+        CLIENT_ID: 'dev_customer_password',
+        CLIENT_SECRET: 'rdkbKVM7neKs15wX0YgOzQjTZ8JF4SRNYs-KAjNr',
         OAUTH2_URL: 'http://localhost:51928/',
         TANNER_PUBLIC_SITE_URL: 'http://www.kunder.cl/tannercl/'
     };
