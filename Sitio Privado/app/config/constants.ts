ba2fd96a6ad3b749f7752e18c94fdcfe5197b37a
José Ignacio Navarro <josencv@gmail.com>
2017-04-05 18:32:35 -0300

    Fixed login url

--- Sitio Privado/app/config/constants.ts
+++ Sitio Privado/app/config/constants.ts
@@ -6,7 +6,7 @@ module app.config {
         CLIENT_SECRET: 'rdkbKVM7neKs15wX0YgOzQjTZ8JF4SRNYs-KAjNr',
         OAUTH2_URL: 'https://oauthdesa.tanner.cl/',
         TANNER_PUBLIC_SITE_URL: 'http://www.kunder.cl/tannercl/',
-        TANNER_AUTHENTICATION_API: 'https://proxylogindesa.scm.azurewebsites.net/api/authentication/',
+        TANNER_AUTHENTICATION_API: 'https://proxylogindesa.azurewebsites.net/api/authentication/',
         REQUIRED_GROUP: 'clientescorredora'
     };
 }
