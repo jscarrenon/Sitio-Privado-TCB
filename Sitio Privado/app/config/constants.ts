d2b9e45b534d74dffa1159c6b2ec89b39ea134c5
beatrizsandoval <beatrizsandoval.externo@tanner.cl>
2017-05-03 18:28:02 -0300

    se corrige variable

--- Sitio Privado/app/config/constants.ts
+++ Sitio Privado/app/config/constants.ts
@@ -15,6 +15,6 @@ module app.config {
 		OAUTH2_URL: 'https://oauthqa.tanner.cl/',
 		TANNER_PUBLIC_SITE_URL: 'http://sitiopublicoqa.tanner.cl/',
 		TANNER_AUTHENTICATION_API: 'https://proxyloginqa.azurewebsites.net/api/authentication/',
-		REQUIRED_GROUP: 'ClientesCorredora_cliente'
+		REQUIRED_GROUP: 'ClientesCorredora'
     };
 }
